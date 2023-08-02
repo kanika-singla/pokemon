10da6feaaeb85f01adee7daf01c4406bbbb0c9b3
Kanika Singla <kanika-singla@outlook.com>
2023-08-02 17:48:43 +0200

    primaryrequirements

--- dumpPokemonDataService.js
+++ dumpPokemonDataService.js
@@ -15,7 +15,7 @@ export async function getAllPokemons() {
   let promises = [];
 
   console.log(new Date().getTime());
-  let pokemons = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=500');
+  let pokemons = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=5');
     const totalPokemons = pokemons.data.count;
     console.log(totalPokemons);
     for (let index = 0; index < pokemons.data.results.length; index++) {
---
+++ primaryRequirements.js
@@ -0,0 +1,43 @@
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = "mongodb+srv://import_user:<PASSWORD>@<SANDBOX>.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const aggregationPipeline = [
+    {
+      '$match': {
+        'game_indices.version.name': {
+            '$in': [
+            'red', 'leafgreen', 'blue', 'green', 'white'
+          ]
+        }
+      }
+    },
+    {
+        '$project': {
+            'name': 1,
+            'id': 1,
+            'base_experience': 1,
+            'weight': 1,
+            'height': 1 ,
+            'order': 1,
+            'game_indices.version.name': 1
+        }
+    }
+  ];
+
+
+  const pokemonCollection = client.db('pokemon').collection('pokemon');
+  const cursor = pokemonCollection.aggregate(aggregationPipeline);
+  const result = await cursor.toArray();
+  console.log(JSON.stringify(result));
+  console.log(result.length);
+  await client.close();
